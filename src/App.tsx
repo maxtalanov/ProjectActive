e664b8edec9bcf26f6a645850331c43970610711
Maxim Talanov <mail@maxtalanov.ru>
2024-10-16 01:32:56 +0300

    feat: (1.6): Добавлен и настроен routing: - линивая подгрузка - тестовые компоненты роутинга - отработка загрузки асинхронной подгрузки чанков (роутов)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import {Counter} from "./Components/Counter";
+
+import {Link, Route, Routes} from "react-router-dom";
 import "./index.scss";
+import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
+import {Suspense} from "react";
 
 export const App = () => {
     return (
        <div className={'app'}>
-           Точка входа в app
-           <Counter />
+           <Link to={'/'}>Главная</Link>
+           <Link to={'/about'}>О сайте</Link>
+
+           <Suspense fallback={<div>Loading...</div>}>
+               <Routes>
+                   <Route path={'about'} element={<AboutPageAsync />}/>
+                   <Route path={'/'} element={<MainPageAsync />}/>
+               </Routes>
+           </Suspense>
        </div>
     )
 }
