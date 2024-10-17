5b07e5736291532ed52eaba3813aa33dec1d94c5
Maxim Talanov <mail@maxtalanov.ru>
2024-10-17 15:55:03 +0300

    feat: (1.10.1): webpack и ts-конфинг - Настройка относительных путей - Подготовка для методологии FCD

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,14 @@
 import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers({paths}: BuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        mainFiles: ['index'],
+        modules: [
+            paths.src,
+            'node_modules'
+        ],
+        alias: {},
     }
 }
