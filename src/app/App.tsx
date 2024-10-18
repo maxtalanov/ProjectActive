aea5f3e9d26b0749920589c6e759a7bf4b3d05d3
Maxim Talanov <mail@maxtalanov.ru>
2024-10-18 03:44:37 +0300

    ref: (1.10.2): преведение моодуля App и Theme в соответствующие каталоги по FCD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {classNames} from "./helpers/classNames/classNames";
-import {useTheme} from "./theme/useTheme";
+import {AboutPageAsync} from "pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "pages/MainPage/MainPage.async";
+import {classNames} from "helpers/classNames/classNames";
+import {useTheme} from "./providers/ThemeProvider/lib/useTheme";
 import './styles/index.scss';
 
 const App = () => {
