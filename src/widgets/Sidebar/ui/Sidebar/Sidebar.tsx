56dde568eab17a4964248b75622bc2d3760d870b
Maxim Talanov <mail@maxtalanov.ru>
2024-10-25 04:11:34 +0300

    feat(2.4.7): Добавлена кнопка изменения языка

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { classNames } from 'shared/lib/classNames/classNames';
 import {ThemeSwitcher} from "shared/ui/ThemeSwither";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -21,7 +22,7 @@ export const Sidebar = ({className}: SidebarProps) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                {/* <LangSwitcher /> */}
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
