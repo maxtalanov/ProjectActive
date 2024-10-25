f884a802d18bc7d95fa070fa0de588a75dcff7cc
Maxim Talanov <mail@maxtalanov.ru>
2024-10-25 04:07:54 +0300

    ref(2.4.5): Добавлена оптимизация компонета App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,18 +3,22 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {Suspense} from "react";
 import './styles/index.scss';
+
 
 const App = () => {
     const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback={null}>
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
