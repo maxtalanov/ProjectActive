429e6701de539d6a0fe6301592a549be280a8477
Maxim Talanov <mail@maxtalanov.ru>
2024-10-30 00:41:57 +0300

    feat(2.6): Настройка babel plugin i18n

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -39,10 +39,30 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
             },
         ],
     }
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ["en", "ru"],
+                            keyAsDefaultValue: true,
+                        }
+                    ],
+                ]
+            }
+        }
+    }
 
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typeScriptLoader,
         cssLoader,
     ]
