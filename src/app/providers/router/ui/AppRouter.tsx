fb96bef446a22e094db025df097971222aa6ccce
Maxim Talanov <mail@maxtalanov.ru>
2024-11-04 16:04:54 +0300

    feat(2.9.4): Добавлена страница Not Found Page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-
 import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { RouteConfig } from 'shared/config/routerConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(RouteConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
+    <Routes>
+        {Object.values(RouteConfig).map(({ element, path }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
                         <div className="page-wrapper">
                             {element}
                         </div>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
 );
 
 export default AppRouter;
