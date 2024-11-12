f671a43863572cb4b91114f529838cdbb9607548
Maxim Talanov <mail@maxtalanov.ru>
2024-11-13 02:15:38 +0300

    ref(4.6): TS2307: добавлены новые систрии

--- src/shared/config/storybook/themeDecorator/themeDecorator.tsx
+++ src/shared/config/storybook/themeDecorator/themeDecorator.tsx
@@ -1,7 +1,7 @@
 import { Story } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 
-export const themeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
     <div className={`app ${theme}`}>
         <StoryComponent />
     </div>
