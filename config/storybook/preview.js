1c4a866957a765cae4bbebd850cdced76869524c
Maxim Talanov <mail@maxtalanov.ru>
2024-11-13 02:11:09 +0300

    ref(4.5): мелкие правки

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,8 +1,8 @@
 import { addDecorator } from '@storybook/react';
 import { styleDecorator } from '../../src/shared/config/storybook/styleDecorator/styleDecorator';
-import { themeDecorator } from '../../src/shared/config/storybook/themeDecorator/themeDecorator';
-import { Theme } from '../../src/app/providers/ThemeProvider';
+import { ThemeDecorator } from '../../src/shared/config/storybook/themeDecorator/themeDecorator';
 import { routerDecorator } from '../../src/shared/config/storybook/routerDecorator/routerDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -15,5 +15,5 @@ export const parameters = {
 };
 
 addDecorator(styleDecorator);
-addDecorator(themeDecorator(Theme.LIGHT));
+addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(routerDecorator);
