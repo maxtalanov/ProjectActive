0e6fa68d013a287a32ee1738f19e63919b901df7
Maxim Talanov <mail@maxtalanov.ru>
2024-12-11 03:18:39 +0300

    feat(16.7): модуль коунтер

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,37 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+
+    const counterValue = useSelector(getCounterValue);
+
+    const increment = (): void => {
+        dispatch(counterActions.increment());
+    };
+    const decrement = (): void => {
+        dispatch(counterActions.decrement());
+    };
+
+    return (
+        <div>
+            <h1>
+                {`${t('Счетчик')}: ${counterValue}`}
+            </h1>
+            <Button
+                onClick={increment}
+            >
+                {t('Увеличить')}
+            </Button>
+            <Button
+                onClick={decrement}
+            >
+                {t('Уменьшить')}
+            </Button>
+        </div>
+    );
+};
