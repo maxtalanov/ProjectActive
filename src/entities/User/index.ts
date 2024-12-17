cb83903706210c9de9a0b43c1282fa32e0fff3b7
Maxim Talanov <mail@maxtalanov.ru>
2024-12-17 04:30:18 +0300

    feat(19.3):  Добавил Redux-логику для управления состоянием User

---
+++ src/entities/User/index.ts
@@ -0,0 +1,9 @@
+import { userActions, userReducer } from './model/slice/userSlice';
+import type { UserSchema, User } from './model/types/user';
+
+export {
+    userReducer,
+    userActions,
+    UserSchema,
+    User,
+};
