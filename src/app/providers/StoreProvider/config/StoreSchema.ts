6bcc2fa31ae821e2366f05882c49dba82482fd47
Maxim Talanov <mail@maxtalanov.ru>
2025-01-05 16:08:53 +0300

    feat(34.4): Добавлена новая схема для StateSchema

--- src/app/providers/StoreProvider/config/StoreSchema.ts
+++ src/app/providers/StoreProvider/config/StoreSchema.ts
@@ -1,7 +1,9 @@
 import { CounterSchema } from 'entities/Counter';
 import { UserSchema } from 'entities/User';
+import { LoginSchema } from 'features/AuthByUcerName';
 
 export interface StateSchema {
     counter: CounterSchema,
     user: UserSchema,
+    loginForm?: LoginSchema,
 }
