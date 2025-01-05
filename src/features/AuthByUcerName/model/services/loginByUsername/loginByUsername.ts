4e6dfc6a1b85783ec5f00af52c8263246dff15b2
Maxim Talanov <mail@maxtalanov.ru>
2025-01-05 16:19:08 +0300

    feat(34.7): Добавлен асинхронный экшен для получения Юсера через API

---
+++ src/features/AuthByUcerName/model/services/loginByUsername/loginByUsername.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { User, userActions } from 'entities/User';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import i18n from 'shared/config/i18n/i18n';
+
+interface LoginByUsernameProps {
+    username: string;
+    password: string;
+}
+
+export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
+    'login/loginByUsername',
+    async (authData, thunkAPI) => {
+        try {
+            const response = await axios.post<User>('http://localhost:8000/login', authData);
+
+            if (!response.data) {
+                throw new Error();
+            }
+
+            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+            thunkAPI.dispatch(userActions.setAuthData(response.data));
+
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(i18n.t('Вы ввели не корректные данные'));
+        }
+    },
+);
