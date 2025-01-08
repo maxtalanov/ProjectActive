07d79f344949b250ed58dc610a935f51bff5f38b
Maxim Talanov <mail@maxtalanov.ru>
2025-01-08 20:27:02 +0300

    feat(34.12):  расширен фуекционал компонента LoginForm

--- src/features/AuthByUcerName/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUcerName/ui/LoginForm/LoginForm.stories.tsx
@@ -3,6 +3,9 @@ import {
     ThemeDecorator,
 } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import {
+    StoreDecorator,
+} from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { LoginForm } from './LoginForm';
 
 export default {
@@ -19,7 +22,21 @@ const Template: ComponentStory<typeof LoginForm> = (args) => (
 
 export const Primary = Template.bind({});
 Primary.args = {};
+Primary.decorators = [StoreDecorator({
+    loginForm: {
+        username: 'login',
+        password: 'password',
+    },
+})];
 
 export const PrimaryDark = Template.bind({});
 PrimaryDark.args = {};
-PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+PrimaryDark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        loginForm: {
+            username: 'login',
+            password: 'password',
+        },
+    }),
+];
