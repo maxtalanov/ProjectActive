d4372123790d8fbd1d7e3c4ba162a251c8558706
Maxim Talanov <mail@maxtalanov.ru>
2025-01-08 20:24:29 +0300

    ref(34.10): Добавлены изменения reducer User - public API - schema - reducer

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,9 +1,13 @@
-import { userActions, userReducer } from './model/slice/userSlice';
-import type { UserSchema, User } from './model/types/user';
-
 export {
-    userReducer,
     userActions,
+    userReducer,
+} from './model/slice/userSlice';
+
+export type {
     UserSchema,
     User,
-};
+} from './model/types/user';
+
+export {
+    getUserAuthData,
+} from './model/selectors/getUserAuthData/getUserAuthData';
