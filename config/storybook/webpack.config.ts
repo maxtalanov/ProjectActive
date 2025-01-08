ec9a0b65595a378be83d4ca2258e838093e6ffb1
Maxim Talanov <mail@maxtalanov.ru>
2025-01-08 20:20:51 +0300

    ref(34.9): Скорректированы импырты и расширен конфиг

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,6 +1,5 @@
-import * as webpack from 'webpack';
-import * as path from 'path';
-import { RuleSetRule } from 'webpack';
+import webpack, { DefinePlugin, RuleSetRule } from 'webpack';
+import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
@@ -28,6 +27,10 @@ export default ({ config }: {config: webpack.Configuration}) => {
         use: ['@svgr/webpack'],
     });
     config.module.rules.push(buildCssLoader(true));
+
+    config.plugins.push(new DefinePlugin({
+        __IS_DEV__: true,
+    }));
 
     return config;
 };
