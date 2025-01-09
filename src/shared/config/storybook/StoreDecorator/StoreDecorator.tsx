28c27db250552b78d1a49f9be97695f532524492
Maxim Talanov <mail@maxtalanov.ru>
2025-01-09 03:52:42 +0300

    fix(34.18): Исправлена ошибка в имене пропса для store

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -3,7 +3,7 @@ import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { DeepPartial } from '@reduxjs/toolkit';
 
 export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => (
-    <StoreProvider initalState={state}>
+    <StoreProvider initialState={state}>
         <StoryComponent />
     </StoreProvider>
 );
