c04a017a24f09bde5407fc3bb5997e7ee46d29f5
Maxim Talanov <mail@maxtalanov.ru>
2025-01-17 02:21:08 +0300

    feat(37.3): Добавить новую страницу

---
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ProfileSchema } from '../types/profile';
+
+const initialState: ProfileSchema = {
+    isLoading: false,
+    readonly: false,
+    error: undefined,
+    data: undefined,
+};
+
+export const profileSlice = createSlice({
+    name: 'profile',
+    initialState,
+    reducers: {
+
+    },
+});
+
+export const { actions: profileActions } = profileSlice;
+export const { reducer: profileReducer } = profileSlice;
