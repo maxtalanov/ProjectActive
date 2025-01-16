6695ecb76b13a89e28ad77bf5bbfc521fc52f4d5
Maxim Talanov <mail@maxtalanov.ru>
2025-01-17 02:31:01 +0300

    ref(37.8): Скорректировать компонент sidebar - Оптимизация производительности - Декомпозиция элементов --> вынос items в подкомпонент - добавить модель для items

---
+++ src/widgets/Sidebar/ui/Sidebar/SidebarItem/SidebarItem.tsx
@@ -0,0 +1,28 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import cls from './SidebarItem.module.scss';
+import { SidebarItemType } from '../model/items';
+
+interface SidebarItemProps {
+    item: SidebarItemType;
+    collapsed: boolean;
+}
+
+export const SidebarItem = ({ item, collapsed }: SidebarItemProps) => {
+    const { Icon, path, text } = item;
+    const { t } = useTranslation();
+
+    return (
+        <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={path}
+            className={classNames(cls.item, { [cls.collapsed]: collapsed })}
+        >
+            <Icon className={cls.icon} />
+            <span className={cls.link}>
+                {t(text)}
+            </span>
+        </AppLink>
+    );
+};
