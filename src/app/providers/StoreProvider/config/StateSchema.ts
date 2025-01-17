8117d6b71211008097e09da9749f4c942445b4ab
Maxim Talanov <mail@maxtalanov.ru>
2025-01-18 01:35:40 +0300

    ref(37.9): all

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,5 +1,6 @@
 import { CounterSchema } from 'entities/Counter';
 import { UserSchema } from 'entities/User';
+import { ProfileSchema } from 'entities/Profile';
 import { LoginSchema } from 'features/AuthByUsername';
 import {
     AnyAction, CombinedState, EnhancedStore, Reducer, ReducersMapObject,
@@ -10,6 +11,7 @@ export interface StateSchema {
     user: UserSchema;
 
     // Асинхронные reducers
+    profile?: ProfileSchema;
     loginForm?: LoginSchema;
 }
 
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,17 +1,28 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/component/DynamicModuleLoader/DynamicModuleLoader';
+import { profileReducer } from 'entities/Profile';
 
 interface ProfilePageProps {
     className?: string,
 }
 
+const reducers: ReducersList = {
+    profile: profileReducer,
+};
+
 const ProfilePage = ({ className }: ProfilePageProps) => {
     const { t } = useTranslation();
 
     return (
-        <div className={classNames('', {}, [className])}>
-            { t('ProfilePage')}
-        </div>
+        <DynamicModuleLoader
+            reducers={reducers}
+            removeAfterUnmount
+        >
+            <div className={classNames('', {}, [className])}>
+                {t('ProfilePage')}
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
