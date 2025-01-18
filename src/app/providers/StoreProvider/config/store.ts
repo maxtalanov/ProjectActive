faac71e0a368fcb3fce54d7978944af75c04eb1b
Maxim Talanov <mail@maxtalanov.ru>
2025-01-18 03:44:56 +0300

    ref(38.4): Добавить extra thunk

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,12 +1,17 @@
 import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
+import { $api } from 'shared/api/api';
+import { NavigateOptions } from 'react-router';
+import { To } from 'history';
+
 import { createReducerManager } from './reducerManager';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(
     initialState?: StateSchema,
     asyncReducers?: ReducersMapObject<StateSchema>,
+    navigate?: (to: To, options?: NavigateOptions) => void,
 ) {
     const rootReducers: ReducersMapObject<StateSchema> = {
         ...asyncReducers,
@@ -15,11 +20,19 @@ export function createReduxStore(
     };
 
     const reducerManager = createReducerManager(rootReducers);
-
-    const store = configureStore<StateSchema>({
+    const thunkArg = {
+                    api: $api,
+                    navigate,
+    }
+    const store = configureStore({
         reducer: reducerManager.reduce,
         devTools: __IS_DEV__,
         preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            thunk: {
+                extraArgument: thunkArg,
+            },
+        }),
     });
 
     // @ts-ignore
