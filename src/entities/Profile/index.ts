3b3cf6c829e734f3f8fa56da5ef091bbe21e3051
Maxim Talanov <mail@maxtalanov.ru>
2025-01-20 04:05:18 +0300

    feat(38.11): Добавить в паблик апи - ui - service

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,4 +1,12 @@
 export {
+    ProfileCard,
+} from './ui/ProfileCard/ProfileCard';
+
+export {
+    fetchProfileData,
+} from './model/service/fetchProfileData/fetchProfileData';
+
+export {
     Profile,
     ProfileSchema,
 } from './model/types/profile';
