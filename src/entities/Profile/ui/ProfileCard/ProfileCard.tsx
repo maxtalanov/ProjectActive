fe0823029b3e3366863c06815a61466b64dd200f
Maxim Talanov <mail@maxtalanov.ru>
2025-01-20 04:00:21 +0300

    feat(38.8):  Добавить компонент Profile card

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,53 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { Text } from 'shared/ui/Text/Text';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Input } from 'shared/ui/Input/Input';
+import { getProfileData } from '../../model/selectors/getProfileData/getProfileData';
+import { getProfileReadonly } from '../../model/selectors/getProfileReadonly/getProfileReadonly';
+import { getProfileIsLoading } from '../../model/selectors/getProfileIsLoading/getProfileIsLoading';
+import { getProfileError } from '../../model/selectors/getProfileError/getProfileErorr';
+import cls from './ProfileCard.module.scss';
+
+interface ProfileCardProps {
+    className?: string,
+}
+
+export const ProfileCard = ({ className }: ProfileCardProps) => {
+    const { t } = useTranslation('profil');
+    const data = useSelector(getProfileData);
+    const readonly = useSelector(getProfileReadonly);
+    const error = useSelector(getProfileError);
+    const isLoading = useSelector(getProfileIsLoading);
+    return (
+        <div className={classNames(cls.ProfileCard, {}, [className])}>
+            <div className={cls.header}>
+                <Text
+                    title={t('Профиль')}
+                />
+
+                <Button
+                    className={cls.editBtn}
+                    theme={ButtonTheme.OUTLINE}
+                >
+                    {t('Редактировать')}
+                </Button>
+            </div>
+
+            <div className={cls.data}>
+                <Input
+                    value={data?.first}
+                    placeholder={t('Ваше имя')}
+                    className={cls.input}
+                />
+
+                <Input
+                    value={data?.lastname}
+                    placeholder={t('Ваша фамилия')}
+                    className={cls.input}
+                />
+            </div>
+        </div>
+    );
+};
