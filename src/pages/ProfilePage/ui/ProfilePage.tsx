a51da6907630b49b6ada8b6966a39db9e4edbbcc
Maxim Talanov <mail@maxtalanov.ru>
2025-02-03 04:06:38 +0300

    feat(41.3): Добавлена страница profile

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,9 +1,19 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/component/DynamicModuleLoader/DynamicModuleLoader';
-import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
-import { useEffect } from 'react';
+import {
+    fetchProfileData,
+    getProfileError, getProfileForm, getProfileIsLoading,
+    profileActions,
+    ProfileCard,
+    profileReducer,
+} from 'entities/Profile';
+import { useCallback, useEffect } from 'react';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country/model/types/Country';
+import { ProfilePageHeader } from './ProfilePageHeader/ProfilePageHeader';
 
 interface ProfilePageProps {
     className?: string,
@@ -16,9 +26,46 @@ const reducers: ReducersList = {
 const ProfilePage = ({ className }: ProfilePageProps) => {
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
+    const dataForm = useSelector(getProfileForm);
+    const error = useSelector(getProfileError);
+    const isLoading = useSelector(getProfileIsLoading);
 
     useEffect(() => {
         dispatch(fetchProfileData());
+    }, [dispatch]);
+
+    const onChangeLastname = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ lastname: value || '' }));
+    }, [dispatch]);
+
+    const onChangeFirstname = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ first: value || '' }));
+    }, [dispatch]);
+
+    const onChangeAge = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({
+            age: Number(value || 0),
+        }));
+    }, [dispatch]);
+
+    const onChangeCity = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ city: value || '' }));
+    }, [dispatch]);
+
+    const onChangeAvatar = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ avatar: value || '' }));
+    }, [dispatch]);
+
+    const onChangeUsername = useCallback((value?: string) => {
+        dispatch(profileActions.updateProfile({ username: value || '' }));
+    }, [dispatch]);
+
+    const onChangeCurrency = useCallback((currency?: Currency) => {
+        dispatch(profileActions.updateProfile({ currency }));
+    }, [dispatch]);
+
+    const onChangeCountry = useCallback((country?: Country) => {
+        dispatch(profileActions.updateProfile({ country }));
     }, [dispatch]);
 
     return (
@@ -27,7 +74,21 @@ const ProfilePage = ({ className }: ProfilePageProps) => {
             removeAfterUnmount
         >
             <div className={classNames('', {}, [className])}>
-                <ProfileCard />
+                <ProfilePageHeader />
+                <ProfileCard
+                    data={dataForm}
+                    isLoading={isLoading}
+                    error={error}
+                    onChangeLastname={onChangeLastname}
+                    onChangeFirstname={onChangeFirstname}
+                    onChangeAge={onChangeAge}
+                    onChangeCity={onChangeCity}
+                    onChangeAvatar={onChangeAvatar}
+                    onChangeUsername={onChangeUsername}
+                    onChangeCountry={onChangeCountry}
+                    onChangeCurrency={onChangeCurrency}
+
+                />
             </div>
         </DynamicModuleLoader>
     );
---
+++ src/pages/ProfilePage/ui/ProfilePageHeader/ProfilePageHeader.tsx
@@ -0,0 +1,67 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Text } from 'shared/ui/Text/Text';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { getProfileReadonly, profileActions, updateProfileData } from 'entities/Profile';
+import { useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import cls from './ProfilePageHeader.module.scss';
+
+interface ProfilePageHeaderProps {
+    className?: string,
+}
+
+export const ProfilePageHeader = ({ className }: ProfilePageHeaderProps) => {
+    const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const readonly = useSelector(getProfileReadonly);
+
+    const onEdit = useCallback(() => {
+        dispatch(profileActions.setReadonly(false));
+    }, [dispatch]);
+
+    const onCancelEdit = useCallback(() => {
+        dispatch(profileActions.cancelEdit());
+    }, [dispatch]);
+
+    const onSave = useCallback(() => {
+        dispatch(updateProfileData());
+    }, [dispatch]);
+
+    return (
+        <div className={classNames(cls.ProfilePageHeader, {}, [className])}>
+            <Text
+                title={t('Профиль')}
+            />
+
+            { readonly
+                ? (
+                    <Button
+                        className={cls.editBtn}
+                        theme={ButtonTheme.OUTLINE}
+                        onClick={onEdit}
+                    >
+                        {t('Редактировать')}
+                    </Button>
+                ) : (
+                    <>
+                        <Button
+                            className={cls.editBtn}
+                            theme={ButtonTheme.OUTLINE}
+                            onClick={onCancelEdit}
+                        >
+                            {t('Отменеть')}
+                        </Button>
+                        <Button
+                            className={cls.saveBtn}
+                            theme={ButtonTheme.BACKGROUND_INVERTED}
+                            onClick={onSave}
+                        >
+                            {t('Сохранить')}
+                        </Button>
+                    </>
+                )}
+        </div>
+    );
+};
