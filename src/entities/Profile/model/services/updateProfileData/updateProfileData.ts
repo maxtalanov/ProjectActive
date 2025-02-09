f115456dab2ba9254728d53db84bf6049230d124
Maxim Talanov <mail@maxtalanov.ru>
2025-02-10 01:44:35 +0300

    fix(42.5): loki

--- src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
@@ -30,7 +30,6 @@ export const updateProfileData = createAsyncThunk<
 
                 return response.data;
             } catch (e) {
-                console.log(e);
                 return rejectWithValue([ValidateProfileError.SERVER_ERROR]);
             }
         },
