e62b7e90adbd64300d0c274e4b7b78f041fa401e
Maxim Talanov <mail@maxtalanov.ru>
2025-03-12 15:01:28 +0300

    feat(49.14): Добавить новое entiti -> Comment - Public API - UI - Types

---
+++ src/entities/Comment/ui/CommentList/CommentList.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CommentList } from './CommentList';
+
+export default {
+    title: 'СЛОЙ/CommentList',
+    component: CommentList,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof CommentList>;
+
+const Template: ComponentStory<typeof CommentList> = (args) => <CommentList {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+    // Аргументы
+};
